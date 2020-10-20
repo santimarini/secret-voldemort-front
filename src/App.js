89921dc413570b46290090ce7cb755ed03626732
Santi Marini <santiagomarini44@gmail.com>
2020-10-20 20:10:22 -0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import SignUp from './_components/auth/SignUp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+        <Switch>
+            <Route path='/signup' component={SignUp}/>
+            <Route path='/login' component={LogIn}/>
+        </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
