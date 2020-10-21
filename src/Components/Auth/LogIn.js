4fd9b6d2354206208bc4f6d53e3c95ef8f64b252
joaco <joacofracchia@gmail.com>
2020-10-21 18:52:05 -0300

    validation

--- src/Components/Auth/LogIn.js
+++ src/Components/Auth/LogIn.js
@@ -1,36 +1,75 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+
+const server_uri = "sv";
+const DATA_FORMAT = {
+  min: 4,
+  max: 16
+}
 
 function LogIn() {
-    const [userInfo, setUserInfo] = useState({
-      email: '',
-      password: ''
-    })
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e) => {
-      setUserInfo({...userInfo, [e.target.name]: e.target.value})
-    }
-    
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      console.log(userInfo)
+  // get all user inputs
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  // send to the back the request when user accepts the form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      userInfo.password.length < DATA_FORMAT.min ||
+      DATA_FORMAT.max < userInfo.password.length
+    ) {
+      return alert(
+        "Invalid password. \nPlease insert a correct one between " +
+          DATA_FORMAT.min +
+          " and " +
+          DATA_FORMAT.max +
+          " characters."
+      );
     }
 
-    return (
-      <div className="container">
-        <form onSubmit={handleSubmit} className="white">
-          <h2 className="grey-text text-darken-3">Login for play!</h2>
-          <div className="input-field">
-            <label htmlFor="text">Email: </label>
-            <input type="email" id="username" name="email" onChange={handleChange}/>
-          </div>
-          <div className="input-field">
-            <label htmlFor="email">Password: </label>
-            <input type="password" id="email" name="password" onChange={handleChange}/>
-          </div>
-          <button className="btn pink lighten-1 z-depth-0"> Log In </button>  
-        </form>
-      </div>
-    )
+    axios({
+      method: "post",
+      url: server_uri,
+      data: userInfo,
+    });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="white">
+        <h2 className="grey-text text-darken-3">Login for play!</h2>
+        <div className="input-field">
+          <label htmlFor="text">Email: </label>
+          <input
+            type="email"
+            id="username"
+            name="email"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="email">Password: </label>
+          <input
+            type="password"
+            id="email"
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button className="btn pink lighten-1 z-depth-0"> Log In </button>
+      </form>
+    </div>
+  );
 }
 
 export default LogIn;
