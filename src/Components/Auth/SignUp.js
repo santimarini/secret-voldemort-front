a2a67ae8365547c36387b06789101324d39848ac
Santi Marini <santiagomarini44@gmail.com>
2020-10-21 18:18:15 -0300

    userInfo checks and alerts

--- src/Components/Auth/SignUp.js
+++ src/Components/Auth/SignUp.js
@@ -1,7 +1,16 @@
-import React, { Component, useState }  from 'react'
+import React, { useState }  from 'react'
+import axios from 'axios'
+
+//username and password lengths
+const DATA_FORMAT = {
+	min: 4,
+	max: 16
+}
+const ENDPOINT_SU = 'http://localhost:3000/signup'
 
 
 function SignUp() {
+
   const [userInfo, setUserInfo] = useState({
     username: '',
     email: '',
@@ -15,29 +24,57 @@ function SignUp() {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(userInfo)
+    //check userInfo length
+    try {
+	    if (userInfo.username.length < DATA_FORMAT.min ||
+	     DATA_FORMAT.max < userInfo.username.length)
+	    	throw Error("username")
+	    if (userInfo.password.length < DATA_FORMAT.min ||
+	     DATA_FORMAT.max < userInfo.password.length)
+	    	throw Error("password")
+	  }
+
+	  catch(err){
+	  	alert("Invalid " + err.message + ". \nPlease insert a correct " 
+	  		+ err.message + " between " + DATA_FORMAT.min +
+	  		" and " + DATA_FORMAT.max + " characters.")
+	  }
+		//if all ok, send post request to backend
+    axios.post(ENDPOINT_SU, userInfo) 
+    .then(response => {
+      alert("Welcome " + userInfo.username + "!" + " Log in for play!")
+    })
+    .catch(error=> {
+      alert(error)
+    })
+
   }
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit} className="white">
+  	<div class className="container">
+			<form onSubmit={handleSubmit} className="white">
         <h5 className="grey-text text-darken-3">Sign up for play!</h5>
         <div className="input-field">
           <label htmlFor="text">Username: </label>
-          <input type="text" id="username" name="username" onChange={handleChange}/>
+          <input type="text" id="username" name="username" 
+          	onChange={handleChange} required/>
         </div>
         <div className="input-field">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" onChange={handleChange}/>
+          <input type="email" id="email" name="email" 
+          	onChange={handleChange} required/>
         </div>
         <div className="input-field">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" onChange={handleChange}/>
+          <input type="password" id="password" name="password" 
+          	onChange={handleChange} required/>
         </div>
         <div className="input-field">
           <button className="btn pink lighten-1 z-depth-0"> Sign Up </button>
         </div>
       </form>
     </div>
+  	
   );
 
 }
