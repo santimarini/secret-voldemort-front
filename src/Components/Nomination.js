644be9f01540ad656fff3131462d64d5eb77d9b3
santi marini <santiagomarini44@gmail.com>
2020-10-31 20:43:34 -0300

    all players advance to voting

--- src/Components/Nomination.js
+++ src/Components/Nomination.js
@@ -5,43 +5,61 @@ import axios from 'axios'
 function Nomination(props) {
 
     const [gameInfo, setGameInfo] = useState({
-    game_name: '', 
-    minister: '',
-    players: [],
-  })
-    const [nominationInfo, setNominationInfo] = useState({
-    director: ''
-})
+      game_name: '',
+      minister: {},
+      players: [],
+    })
+    const [nominationInfo] = useState({
+        director: ''
+    })
+
 
     const handleSubmit = async (e) => {
         nominationInfo.director = document.getElementById("candidate").value
-        try{
-            console.log(nominationInfo.director)
-            let response = await axios.put(`http://localhost:8000/game?game_name=${gameInfo.game_name}&dir=${nominationInfo.director}`)
+        try {
+            await axios.put(`http://localhost:8000/game?game_name=${gameInfo.game_name}&dir=${nominationInfo.director}`)
             props.handleCount(2)
         }
         catch (err) {
             alert(err)
         }
-  }
+    }
  
-  useEffect(() => {
-      async function onElection() {
-        gameInfo.game_name = props.game_name
-        try {     
-          let response = await axios.post(`http://localhost:8000/new_turn?game_name=${gameInfo.game_name}`)
-          setGameInfo({...gameInfo, minister: response.data.minister,
-            players: response.data.players })
-        }
-        catch (error) {
-          alert(error)
-        }
-      }   
+
+    useEffect(() => {
+        async function onElection() {
+          gameInfo.game_name = props.game_name
+          try {     
+              let response = await axios.post(`http://localhost:8000/next_turn?game_name=${gameInfo.game_name}`)
+              setGameInfo({...gameInfo, minister: response.data.minister,
+                  players: response.data.players })
+          }
+          catch (error) {
+              alert(error)
+          }
+        }   
         onElection()  
     }, []);  
-  
-    if (localStorage.getItem("email") === gameInfo.minister.user1) {
-        return(
+
+
+    var interval = null
+
+    async function askIsNominated() {
+        try {
+            let response = await axios.get(`http://localhost:8000/phase?game_name=${props.game_name}`)
+            if (response.data.phase_game === 2) {
+                clearInterval(interval)
+                props.handleCount(2)
+            }
+        }
+        catch (err) {
+            alert(err)
+        }
+    }
+   
+    return(
+        <div>
+        {localStorage.getItem("email") === gameInfo.minister.user1 ? (
             <div class className="container">
                 <div class className="notification">
                     You have been nominated as Minister of Magic.
@@ -51,17 +69,17 @@ function Nomination(props) {
                     { gameInfo.players.map((player) => <option value={player.id}> { player.username } </option>) }
                 </select>
                 <button onClick={handleSubmit}> Nominate </button>
-                
             </div>
-        )   
-  }
-  else {
-      return(
-          <div>
-          "The nominated minister" ${gameInfo.minister.username}"is choosing director..."
-          </div>
-      )   
-  }
+        ) : interval = setInterval(function() {
+          askIsNominated();
+          }, 6000)}
+          {localStorage.getItem("email") !== gameInfo.minister.user1 &&
+              <div>
+                  The nominated minister {gameInfo.minister.username} is choosing director...
+              </div>
+          }
+       }
+      </div>)
 }
 
 export default Nomination;
