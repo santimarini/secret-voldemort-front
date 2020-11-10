720a5bdd3bd3136bbab5ba6ba6fde67e43c2ea95
Santi Marini <santiagomarini44@gmail.com>
2020-11-10 17:06:50 -0300

    logout added, distribuited buttons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,9 +6,9 @@ import {
 } from 'mdbreact';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-import { getToken, getAlias } from '../Util/HelperFunctions';
+import { getToken, getAlias, getEmail } from '../Util/HelperFunctions';
 
-function Navbar() {
+function Navbar(props) {
   const [token, setToken] = useState(getToken());
 
   const [isOpen, setIsOpen] = useState(false);
@@ -16,20 +16,22 @@ function Navbar() {
   const toggleCollapse = () => {
     setIsOpen(isOpen);
   };
+   
+  const logout = (e) => {
+    localStorage.clear();
+    window.location.href = "/";
+  }
 
   return (
     <div>
       { token ? (
         <div className="logged-user-navbar">
           <MDBNavbar className="navbar-1" color="default-color" dark expand="md">
-            <MDBNavbarBrand>
-              <strong className="white-text">Voldemort</strong>
-            </MDBNavbarBrand>
             <MDBNavbarToggler onClick={toggleCollapse} />
             <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
               <MDBNavbarNav left>
-                <MDBNavItem active>
-                  <MDBNavLink to="">Home</MDBNavLink>
+                <MDBNavItem active >
+                  <MDBNavLink className="home-navbar-1" to="">Home</MDBNavLink>
                 </MDBNavItem>
 
                 <MDBNavItem className="play-button">
@@ -37,20 +39,26 @@ function Navbar() {
                     <MDBDropdownToggle nav caret>
                         <div className="d-none d-md-inline">Play!</div>
                       </MDBDropdownToggle>
-                    <MDBDropdownMenu className="dropdown-default">
-                        <MDBDropdownItem tag={Link} to="/play">Create game</MDBDropdownItem>
-                        <MDBDropdownItem tag={Link} to="/games">Join game</MDBDropdownItem>
+                    <MDBDropdownMenu className="dropdown-play">
+                        <MDBDropdownItem tag={Link} to="/play"><p className="create-game-navbar">Create game</p></MDBDropdownItem>
+                        <MDBDropdownItem tag={Link} to="/games"><p className="join-game-navbar">Join game</p></MDBDropdownItem>
                       </MDBDropdownMenu>
                   </MDBDropdown>
                 </MDBNavItem>
-                <MDBNavItem>
-                  <MDBDropdown>
-                    <MDBDropdownToggle nav caret>
-                        <div className="d-none d-md-inline">{getAlias()}</div>
-                      </MDBDropdownToggle>
-                    <MDBDropdownMenu className="dropdown-default">
-                        <MDBDropdownItem tag={Link} to="/profile"> Profile</MDBDropdownItem>
-                      </MDBDropdownMenu>
+                <MDBNavItem className="alias" right>
+                    <MDBDropdown> 
+                    <MDBDropdownToggle caret>
+                      <div className="d-none d-md-inline">{getAlias()}</div>
+                    </MDBDropdownToggle>
+                    <div className="alias-desplegated"> 
+                    <MDBDropdownMenu >
+                      <p class="text-lowercase" className="email-navbar"> {getEmail()} </p>
+                      <MDBDropdownItem tag={Link} to="/profile"> Profile</MDBDropdownItem>
+                      <MDBDropdownItem onClick={logout}> 
+                        Logout 
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                    </div>
                   </MDBDropdown>
                 </MDBNavItem>
               </MDBNavbarNav>
@@ -59,15 +67,12 @@ function Navbar() {
         </div>
       ) : (
         <div>
-          <MDBNavbar className="navbar-1" color="default-color" dark expand="md">
-            <MDBNavbarBrand>
-              <strong className="white-text">Voldemort</strong>
-            </MDBNavbarBrand>
+          <MDBNavbar className="navbar-2" color="default-color" dark expand="md">
             <MDBNavbarToggler onClick={toggleCollapse} />
             <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
               <MDBNavbarNav right>
                 <MDBNavItem active>
-                  <MDBNavLink to="">Home</MDBNavLink>
+                  <MDBNavLink className="home-navbar-2" to="">Home</MDBNavLink>
                 </MDBNavItem>
                 <MDBNavItem active>
                   <MDBNavLink to="/login">Login</MDBNavLink>
--- src/Util/HelperFunctions.js
+++ src/Util/HelperFunctions.js
@@ -1,3 +1,5 @@
+import jwtDecode from 'jwt-decode';
+
 //helper functions to get data from a user session
 export function getToken() {
 
@@ -6,7 +8,7 @@ export function getToken() {
 
 export function getEmail() {
 
-  return localStorage.getItem("email");
+  return jwtDecode(getToken()).sub;
 }
 
 export function getAlias() {
